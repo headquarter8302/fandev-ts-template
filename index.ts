1c7db6f6050bf9207c7d0cb9572a4eb1d6662d44
Markus <65797058+headquarter8302@users.noreply.github.com>
2025-07-13 03:50:23 -0300

    Update index.ts
    Signed-off-by: Markus <65797058+headquarter8302@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 export { }
 declare global {
   interface Window {
+    // feel free to hot-plug any dependencies/resources here
+    mediaWiki: any;
+    jQuery: any;
+    
     // add your own properties/namespaces here (in the 'dev' property)
     dev: any;
   }
